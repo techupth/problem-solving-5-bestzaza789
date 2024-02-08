988823454eec1e8ea3ecffcd000b1aae692ad38f
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-02-08 22:27:51 +0700

    feat:solve problem

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 function runningSum(nums) {
-    
-};
+  let runningSumArray = [];
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    runningSumArray.push(sum);
+  }
+
+  return runningSumArray;
+}
+
+// Test the function
+let nums = [1, 2, 3, 4];
+console.log("Output:", runningSum(nums));
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,20 @@
 function moveZeroes(nums) {
-    
-};
+  let nonZeroPointer = 0;
+
+  // Iterate through the array
+  for (let i = 0; i < nums.length; i++) {
+    // If the current element is non-zero
+    if (nums[i] !== 0) {
+      // Swap the current element with the element at the non-zero pointer
+      let temp = nums[i];
+      nums[i] = nums[nonZeroPointer];
+      nums[nonZeroPointer] = temp;
+      nonZeroPointer++;
+    }
+  }
+}
+
+// Test the function
+let nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+console.log("Output:", nums);
